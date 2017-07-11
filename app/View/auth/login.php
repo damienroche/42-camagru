b5fab199ee012c60d4ed6113bfcbadec9b474671
Damien Roche <damienroche@free.fr>
2017-07-11 22:49:37 +0200

    markup on login form

--- app/View/auth/login.php
+++ app/View/auth/login.php
@@ -3,8 +3,12 @@
   <?php
   $form = new Core\Builder\Form($_POST);
   echo $form->open('/login', 'POST', 'authForms');
+  echo "<label for='email' class='required'><span class='icon-user'></span>";
   echo $form->input('username', 'text', 'Username');
-  echo $form->input('password', 'password');
+  echo "</label>";
+  echo "<label for='password' class='required'><span class='icon-unlock'></span>";
+  echo $form->input('password', 'password', 'Password');
+  echo "</label>";
   echo $form->submit();
   echo $form->close();
   ?>
